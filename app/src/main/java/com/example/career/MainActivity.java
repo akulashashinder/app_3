8bfb7cbd3c2da05d03758dbe72d3d74f80f06f2d
shashi <akulashashinder00@gmail.com>
2023-11-22 13:51:26 +0530

    version1 menu added

--- app/src/main/java/com/example/career/MainActivity.java
+++ app/src/main/java/com/example/career/MainActivity.java
@@ -1,48 +1,85 @@
 package com.example.career;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import android.os.Bundle;
-import android.view.MenuItem;
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import com.google.android.material.navigation.NavigationView;
-
 
 public class MainActivity extends AppCompatActivity {
-
     FirebaseAuth auth;
     Button button;
     TextView textView;
     FirebaseUser user;
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.home) {
+            showToast("Home Clicked");
+            return true;
+        } else if (itemId == R.id.gallery) {
+            showToast("Gallery Clicked");
+            return true;
+        } else if (itemId == R.id.rateus) {
+            showToast("Rateus Clicked");
+            return true;
+        } else if (itemId == R.id.about) {
+            showToast("About Clicked");
+            return true;
+        } else if (itemId == R.id.login) {
+            showToast("Log In Clicked");
+            return true;
+        } else if (itemId == R.id.profile) {
+            showToast("Profile Clicked");
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        auth=FirebaseAuth.getInstance();
-        button=findViewById(R.id.logout);
-        textView =findViewById(R.id.user_details);
-        user= auth.getCurrentUser();
-        if(user == null){
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+
+        if (user == null) {
             Intent intent = new Intent(getApplicationContext(), login.class);
             startActivity(intent);
             finish();
-        }
-        else {
+        } else {
             textView.setText(user.getEmail());
         }
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,8 +87,6 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), login.class);
                 startActivity(intent);
                 finish();
-
             }
         });
-    }
-}
+    }}
---
+++ app/src/main/java/com/example/career/menu.java
@@ -0,0 +1,5 @@
+package com.example.career;
+
+public class menu {
+
+}
