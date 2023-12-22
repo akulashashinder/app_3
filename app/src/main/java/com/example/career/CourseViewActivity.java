305e18b97f1f1e2e35425d370f926ea69afb17bf
akulashashinder <akulashashinder00@gmail.com>
2023-12-22 20:42:27 +0530

    semi final version 2

--- app/src/main/java/com/example/career/CourseViewActivity.java
+++ app/src/main/java/com/example/career/CourseViewActivity.java
@@ -1,8 +1,27 @@
 package com.example.career;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CourseViewActivity extends AppCompatActivity {
 
@@ -10,5 +29,50 @@ public class CourseViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_course_view);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#001F3F")));
+        PDFView pdfView = findViewById(R.id.pdfView);
+        String url="https://d2cyt36b7wnvt9.cloudfront.net/exams/wp-content/uploads/2022/03/30234614/JEE-Main-Syllabus-2022.pdf";
+        String topicName="Sheshi";
+        TextView textView=findViewById(R.id.content_title);
+        textView.setText(topicName);
+        Log.i("my url",url);
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(new Runnable() {
+            InputStream inputStream=null;
+            @Override
+            public void run() {
+
+                //Background work here
+                try{
+                    URL url2=new URL(url);
+                    HttpURLConnection urlConnection=(HttpURLConnection) url2.openConnection();
+                    if(urlConnection.getResponseCode()==200){
+                        inputStream=new BufferedInputStream(urlConnection.getInputStream());
+
+                    }
+                } catch (IOException e){
+                    e.printStackTrace();
+
+                }
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (inputStream != null) {
+                            pdfView.fromStream(inputStream).load();
+                        } else {
+                            // Handle the case where inputStream is null (e.g., show an error message)
+                            Toast.makeText(CourseViewActivity.this, "Unable to Display Content", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+            }
+        });
+
+
     }
+
 }
